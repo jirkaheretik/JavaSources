288d8012ea8846dfeb77d5af28e07dc8b41a196e
Jirka Braza <j@braza.cz>
2023-05-25 14:28:50 +0200

    euler.P1Multiples - simplified a condition.

--- src/main/java/cz/braza/euler/P1Multiples.java
+++ src/main/java/cz/braza/euler/P1Multiples.java
@@ -5,7 +5,7 @@ public class P1Multiples {
         // sum of multiples of 3 or 5 below 1000
         int sum = 0;
         for (int i = 1; i < 1000; i++)
-            if ((i % 3 == 0) || (i % 5 == 0))
+            if (i % 3 == 0 || i % 5 == 0)
                 sum += i;
         System.out.println("Sum of multiples is " + sum);
     }
