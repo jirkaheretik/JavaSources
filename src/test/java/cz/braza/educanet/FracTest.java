8642b9345a9b39b33f65245bc5c791392af88f83
Jirka Braza <j@braza.cz>
2023-08-07 11:16:54 +0200

    Added equals unit test

---
+++ src/test/java/cz/braza/educanet/FracTest.java
@@ -0,0 +1,35 @@
+package cz.braza.educanet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class FracTest {
+
+    @Test
+    void fromInt() {
+    }
+
+    @Test
+    void plus() {
+    }
+
+    @Test
+    void minus() {
+    }
+
+    @Test
+    void times() {
+    }
+
+    @Test
+    void divide() {
+    }
+
+    @Test
+    void reciprocal() {
+    }
+
+    @Test
+    void toReal() {
+    }
+}
