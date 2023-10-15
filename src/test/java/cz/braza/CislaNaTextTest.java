8c2a643998185536355868761510a8fca4546486
Jirka Braza <j@braza.cz>
2023-10-15 11:27:15 +0200

    OOP skoleni - testy

---
+++ src/test/java/cz/braza/CislaNaTextTest.java
@@ -0,0 +1,15 @@
+package cz.braza;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CislaNaTextTest {
+
+    @Test
+    void testSub1000ToCzech() {
+    }
+
+    @Test
+    void testIntToCzech() {
+    }
+}
--- src/test/java/cz/braza/oop/KalkulackaTest.java
+++ src/test/java/cz/braza/oop/KalkulackaTest.java
@@ -13,9 +13,11 @@ class KalkulackaTest {
 
         kalkulacka = new Kalkulacka(-13, -29);
         assertEquals(-42, kalkulacka.secti(), 0.00001);
+    }
 
+    @Test
+    void sectiStaticky() {
         assertEquals(3, Kalkulacka.secti(1, 2));
-
     }
 
     @Test
@@ -30,10 +32,23 @@ class KalkulackaTest {
 
     @Test
     void vynasob() {
+        Kalkulacka kalkulacka = new Kalkulacka(3, 5);
+        assertEquals(15, kalkulacka.vynasob(), 0.00001);
+
+        kalkulacka = new Kalkulacka(-13, -9);
+        assertEquals(117, kalkulacka.vynasob(), 0.00001);
     }
 
     @Test
     void vydel() {
+        Kalkulacka kalkulacka = new Kalkulacka(15, 3);
+        assertEquals(5, kalkulacka.vydel(), 0.00001);
+
+        kalkulacka = new Kalkulacka(-26, -13);
+        assertEquals(2, kalkulacka.vydel(), 0.00001);
+
+        final Kalkulacka k2 = new Kalkulacka(42, 0);
+        assertThrows(ArithmeticException.class, () -> k2.vydel());
     }
 
     @Test
@@ -41,5 +56,6 @@ class KalkulackaTest {
         assertEquals(4, Kalkulacka.vydelCelociselne(24, 6));
         assertEquals(7, Kalkulacka.vydelCelociselne((int) 15.46, (int) 2.98));
         assertThrows(ArithmeticException.class, () -> Kalkulacka.vydelCelociselne(42, 0));
+
     }
 }
